04979d3f292ef85dfc3a47634867fcc102067bf0
Ryo Fujimura <ryo176999@gmail.com>
2024-03-16 11:36:36 -0700

    card flip animation

--- Memory Game/CardView.swift
+++ Memory Game/CardView.swift
@@ -26,13 +26,23 @@ struct Card{
 struct CardView: View {
     let card: Card
     
-    @State private var isShowingEmoji = false
+    @State private var isShowingEmoji = true
     
     var body: some View {
         ZStack{
             RoundedRectangle(cornerRadius: 25.0)
-                .fill(isShowingEmoji == true ? .red : .blue)
+                .fill(!isShowingEmoji ? Color.blue.gradient : Color.white.gradient)
+            Text(isShowingEmoji ? card.emoji : "")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
         }
+        .frame(width: 100, height: 100)
+        .rotation3DEffect(.degrees(isShowingEmoji ? 180 : 0), axis: (x: 0, y: 1, z: 0))
+        .onTapGesture {
+            withAnimation(.linear(duration: 0.2)){
+                isShowingEmoji.toggle()
+            }
+        }
+        .shadow(color: .black.opacity(0.4), radius: 4, x: -2, y: 2)
     }
 }
 
